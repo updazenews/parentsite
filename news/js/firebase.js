6fafe5f66effc00e90ae2541da767ed7f8bc8dc4
updazenews <updazenews@gmail.com>
2026-02-06 18:43:46 +0200

    Removed G Analytics

--- news/js/firebase.js
+++ news/js/firebase.js
@@ -17,4 +17,3 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
-export const analytics = getAnalytics(app);
